b92eb45de7fce408f196b845fe0a2bf0ed554bd1
Austin F <austindanielfrench@gmail.com>
2017-11-17 11:01:32 -0800

    added http utilities

--- budget/src/server/server.js
+++ budget/src/server/server.js
@@ -11,9 +11,45 @@ const server = express();
 
 server.use(bodyParser.json());
 
-// root users
-server.get('/users');
+// root show accounts
+server.get('/accounts', (req, res) => {
+  Account.find({}, (err, accounts) => {
+    if (err) {
+      res.status(STATUS_SERVER_ERROR).json({
+        error: '!!E: unable to find accounts'
+      });
+    } else {
+      res.status(STATUS_GOOD).json(accounts);
+    }
+  });
+});
+// check account by id
+server.get('/accounts/:id', (req, res) => {
+  const { id } = req.params;
+  User.findById(id, (err, accounts) => {
+    if (err) {
+      res.status(STATUS_SERVER_ERROR).json({
+        error: '!!E: unable to find account with matching ID'
+      });
+    } else {
+      res.status(STATUS_GOOD).json(accounts);
+    }
+  });
+});
+// add account
+server.post('/accounts', (req, res) => {
+  const newAccount = new Account(req.body);
 
+  newAccount.save((err, accounts) => {
+    if (err) {
+      res.status(STATUS_SERVER_ERROR).json({
+        error: '!!E: unable to create account!'
+      });
+    } else {
+      res.status(201).json(accounts);
+    }
+  });
+});
 // plumbing
 mongoose.Promise = global.Promise;
 const connect = mongoose.connect('mongodb://localhost/budget', {
